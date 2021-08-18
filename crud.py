6c8c884b4f5eacdf6fd486230dd0bd7ce5e2f557
sharon zhen <sharonzhen@ucla.edu>
2021-08-18 23:07:43 +0000

    changed model.py drastically and created create functions for crud.py

--- crud.py
+++ crud.py
@@ -1,107 +1,59 @@
 """ CRUDs of model.py """
 
 from datetime import date
-from model import (db, User, Contact, Resume, Work, Course,
-                    Project, Tech, Detail, connect_to_db)
+from model import (db, User, Contact, Stitem, Dytem, Detail, connect_to_db)
 
 
 
 def create_user(email, password):
-    """ create and return user 
-        email:      string(50) unique true
-        password:   string(50) """
+    """ create and return user """
     user = User(login=email, password=password)
     db.session.add(user)
     db.session.commit()
     return user
 
-def create_contact(user_instance, first, last, 
-                    email=None, phone=None, 
-                    github=None, linkedin=None):
-    """ create and return new contact info 
-        user_pk: PK, FK from users.user_pk
-        first:   string(20)
-        last:    string(20) """
-    contact = Contact(user_pk=user_instance.user_pk, first=first, last=last, 
-                        email=email, phone=phone, github=github, 
-                        linkedin=linkedin)
-    user_instance.contact=contact
+def create_contact(user_instance, first, last, loc, email, 
+                    phone=None, github=None, linkedin=None):
+    """ create and return new contact info """
+    contact = Contact(user_id=user_instance.user_id, 
+                        first=first, last=last, 
+                        email=email, phone=phone, 
+                        github=github, linkedin=linkedin, 
+                        user=user_instance)
+    db.session.add(contact)
     db.session.commit()
     return contact
 
-def create_resume(user_instance, 
-    ):
-    """ create and return new resume 
-        user_pk: FK from users.user_pk """
-    resume = None
-    # TODO
-    return resume
-
-def create_work(user_instance, company, role, location, date_from,
-                    date_to=None):
-    """ create and return new work (employment) instance
-        user_pk:    FK from users.user_pk 
-        company:    string(20)
-        role:       string(50)
-        location:   string(50)
-        date_from:  db.DateTime """
-    work = Work(user_pk=user_instance.user_pk, company=company,
-                role=role, location=location, date_from=date_from,
-                date_to=date_to)
-    user_instance.works.append(work)
-    db.seession.commit()
-    return work
-
-def create_course(user_instance, name):
-    """ create and return course 
-        user_pk:    FK from users.user_pk 
-        name:       string(50) """
-    course = Course(user_pk=user_isntance.user_pk,
-                        name=name)
-    user_instance.courses.append(course)
+def create_stitem(user_instance, stable_type, name=None, date=None,
+                    location=None, description=None):
+    """ create and return new stable item """
+    stitem = Stitem(user_id=user_instance.user_id, 
+                    s_type=stable_type, name=name, 
+                    date=date, loc=location, 
+                    des=description)
+    user_instance.stitems.append(stitem)
     db.session.commit()
-    return course
+    return stitem
 
-def create_project(user_instance, name, link=None):
-    """ create and return project 
-        user_pk:    FK from users.user_pk
-        name:       string(50) """
-    project = Project(user_pk=user_instance.user_pk, 
-                        name=name, link=link)
-    user_instance.projects.append(project)
+def create_dytem(user_instance, dynamic_type, name, role=None, location=None,
+                    date_from=None, date_to=None, tags=None):
+    """ create and return new dynamic item """
+    dytem = Dytem(user_id=user_instance.user_id, 
+                    d_type=dynamic_type, name=name, 
+                    role=role, loc=location, 
+                    d_from=date_from, d_to=date_to, 
+                    tags=tags)
+    user_instance.dytems.append(dytem)
     db.session.commit()
-    return project
-
-def create_tech(user_instance, label, values):
-    """ create and return technologies 
-        user_pk:    FK from users.user_pk
-        label:      string(50) i.e. 'proficient in:'
-        value:      text       i.e. 'c++, python, java' """
-    tech = Tech(user_pk=user_isntance.user_pk,
-                label=label, values=values)
-    user_isntance.techs.append(tech)
-    db.session.commit()
-    return tech
+    return dytem
     
-def create_detail(user_instance, description,
-                    work_instance=None, project_instance=None):
-    """ create and return detail for work or project 
-        user_pk:    FK from users.user_pk 
-        bullet:     text
-        work_pk:    FK from works.work_pk
-        project_pk: FK from projects.project_pk """
-    detail = Detail(user_pk=user_instance.user_pk,
-                            bullet=description)
-    if work_instance:
-        work_instance.details.append(detail)
-        db.commit()
-        return detail
-    elif project_instance:
-        project_instance.details.append(detail)
-        db.commit()
-        return detail
-    else:
-        return None
+def create_detail(dytem_instance, description):
+    """ create and return detail for dynamic item """
+    detail = Detail(dytem_id=dytem_instance.dytem_id,
+                    des=description)
+    dytem_isntance.details.append(detail)
+    db.session.commit()
+    return detail
 
 
 
--- model.py
+++ model.py
@@ -3,190 +3,104 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    """ User with primary key, email, password """
+    """ holds login information """
     __tablename__ = "users"
-    user_pk = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    login = db.Column(db.String(50), unique = True)
-    password = db.Column(db.String(50))
+
+    user_id     = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    login       = db.Column(db.String(50), unique=True, nullable=False)
+    password    = db.Column(db.String(50), nullable=False)
     
-    # relationships
-    # one-to-one
-    contact = db.relationship('Contact', uselist=False) 
-    # one-to-many
-    resumes = db.relationship('Resume')                 
-    works = db.relationship('Work', backref = 'user')
-    courses = db.relationship('Course', backref = 'user')
-    projects = db.relationship('Project', backref = 'user')
-    techs = db.relationship('Tech', backref = 'user')
+    ### RELATIONSHIPS ###
+    contact = db.relationship('Contact', uselist=False) # one-to-one
+    stitems = db.relationship('Stitem', backref='user') # one-to-many
+    dytems = db.relationship('Dytem', backref='user')
 
     def __repr__(self):
-        return f"<User {user_pk} created with login: {login}>"
+        return f"<User {user_pk} created: {login}>"
 
 class Contact(db.Model):
-    """data model for user's contact info """
+    """ data model for user's contact info """
     __tablename__ = "contacts"
-    # primary foreign key
-    user_pk = db.Column(db.Integer, db.ForeignKey('users.user_pk'), primary_key=True)
-    # non-nullables
-    first = db.Column(db.String(20), nullable=False) 
-    last = db.Column(db.String(20), nullable=False) 
-    # nullables
-    email = db.Column(db.String(50), nullable=False)
-    phone = db.Column(db.String(20), nullable=True)
-    github = db.Column(db.String(50), nullable=True) 
+    
+    user_id  = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key=True)
+    first    = db.Column(db.String(20), nullable=False) 
+    last     = db.Column(db.String(20), nullable=False) 
+    loc      = db.Column(db.String(20), nullable=False)
+    email    = db.Column(db.String(50), nullable=False)
+
+    phone    = db.Column(db.String(20), nullable=True)
+    github   = db.Column(db.String(50), nullable=True) 
     linkedin = db.Column(db.String(50), nullable=True)
-    # relationships
-    # one-to-one
-    user = db.relationship('User', uselist=False)   
+    
+    ### RELATIONSHIPS ###
+    user = db.relationship('User', uselist=False)
 
     def __repr__(self):
         return f"<Contact for user {user_pk} created>"
 
-class Resume(db.Model):
-    """ data model for user's resumes """ 
-    __tablename__ = "resumes"
-    # primary key
-    resume_pk = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # foreign keys
-    user_pk = db.Column(db.Integer, db.ForeignKey('users.user_pk'), nullable=False)
-    # nullables
-    course_res = db.Column(db.Integer, nullable=True, autoincrement = True)  
-    projects_res = db.Column(db.Integer, nullable=True, autoincrement=True)
-    tech_res = db.Column(db.Integer, nullable=True, autoincrement=True)
-    work_res = db.Column(db.Integer, nullable=True, autoincrement=True)
+class Stitem(db.Model):
+    """ stable items on resume
+    example: 
+        s_type: (1) education, 
+                (2) tech skills, 
+                (3) coursework """ 
+    __tablename__ = "stable_items"
 
-    # relationships
-    # many-to-one
-    user = db.relationship('User')  
-    # many-to-many
-    contact = db.relationship('Contact', backref = 'resumes') 
-    works = db.relationship('Work', secondary = 'resume_work', backref = 'resumes')
-    courses = db.relationship('Course', secondary = 'resume_course', backref = 'resumes')
-    projects = db.relationship('Project', secondary = 'resume_project', backref = 'resumes')
-    techs = db.relationship('Tech', secondary = 'resume_tech', backref = 'resumes')
+    item_id     = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id     = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    s_type      = db.Column(db.Integer, nullable=False)
+
+    name        = db.Column(db.String(100), nullable=True)
+    date        = db.Column(db.Date, nullable=True)
+    loc         = db.Column(db.String(50), nullable=True)
+    des         = db.Column(db.Text, nullable=True) 
+
+    ### RELATIONSHIPS ###
+    # user
 
     def __repr__(self):
         return f"<Resume {resume_pk} generated>"
 
-class Work(db.Model):
-    """ data model for all employment history """
-    __tablename__ = "works"
-    # primary key
-    work_pk = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # foreign key
-    user_pk = db.Column(db.Integer, db.ForeignKey('users.user_pk'), nullable=False)
-    # non-nullables
-    company = db.Column(db.String(20), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
-    location = db.Column(db.String(50), nullable=False)
-    date_from = db.Column(db.DateTime, nullable=False)
-    # nullables
-    date_to = db.Column(db.DateTime, nullable=True)
+class Dytem(db.Model):
+    """ dynamic items on resume 
+    example: 
+        d_type: (1) work experience
+                (2) projects
+                (3) extracurriculars """
+    __tablename__ = "dynamic_items"
+
+    dytem_id    = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id     = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    d_type      = db.Column(db.Integer, nullable=False)
+    name        = db.Column(db.String(100), nullable=False) # bolded
+
+    role        = db.Column(db.String(50), nullable=True) # italicized
+    loc         = db.Column(db.String(50), nullable=True)
+    d_from      = db.Column(db.Date, nullable=True)
+    d_to        = db.Column(db.Date, nullable=True)
+    tags        = db.Column(db.Text, nullable=False)
+    
+    ### RELATIONSHIPS ###
+    # user
+    details = db.relationship('Detail', backref='dytem')
 
     def __repr__(self):
         return f"<Added workplace at {company} for user {user_pk}>"
 
-class Course(db.Model):
-    """ data model for all courses """ 
-    __tablename__ = "courses"
-    # primary key
-    course_pk = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # foreign key
-    user_pk = db.Column(db.Integer, db.ForeignKey('users.user_pk'), nullable=False)
-    # non-nullables
-    name = db.Column(db.String(50), nullable=False)
-    
-    def __repr__(self):
-        return f"<Added course {name} for {user_pk}>"
-
-class Project(db.Model):
-    """ data model for all projects """ 
-    __tablename__ = "projects"
-    # primary key
-    project_pk = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # foreign key
-    user_pk = db.Column(db.Integer, db.ForeignKey('users.user_pk'), nullable=False)
-    # non-nullables
-    name = db.Column(db.String(50), nullable=False)
-    # nullables
-    link = db.Column(db.String(100), nullable=True)
-
-    def __repr__(self):
-        return f"<Added project {name} for user {user_pk}>"
-
-class Tech(db.Model):
-    """ data model for all technologies """ 
-    __tablename__ = "techs"
-    # primary key
-    tech_pk = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # foreign key 
-    user_pk = db.Column(db.Integer, db.ForeignKey('users.user_pk'), nullable=False)
-    # non-nullable
-    label = db.Column(db.String(50), nullable=False) # i.e. proficient in:
-    values = db.Column(db.Text, nullable=False) # i.e. c++, java, etc.
-    
-    def __repr__(self):
-        return f"<Added {tech} for {user_pk}>"
 
 class Detail(db.Model):
     """ data model for all details """ 
     __tablename__ = "details"
-    # primary key
-    detail_pk = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # foreign key
-    user_pk = db.Column(db.Integer, db.ForeignKey('users.user_pk'), nullable=False)
-    # non-nullable
-    bullet = db.Column(db.Text, nullable=False)
-    # foreign keys
-    work_pk = db.Column(db.Integer, db.ForeignKey('works.work_pk'), nullable=True)
-    project_pk = db.Column(db.Integer, db.ForeignKey('projects.project_pk'), nullable=True)
-    # relationships
-    # many-to-one
-    work = db.relationship('Work', backref = 'details')
-    project = db.relationship('Project', backref = 'details')
+    
+    detail_id   = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    dytem_id    = db.Column(db.Integer, db.ForeignKey('dynamic_items.dytem_id'), nullable=False)
+    des         = db.Column(db.Text, nullable=False)
+
+    ### RELATIONSHIPS ###
+    # dytem
 
     def __repr__(self):
-        if work:
-            return f"<Added employment detail for user {user_pk}>"
-        elif project:
-            return f"<Added project detail for user {user_pk}>"
-        else:
-            return f"Added detail for neither employment nor project"
-
-###############################################################
-################ ASSOCIATION TABLES ###########################
-###############################################################
-
-class ResumeCourse(db.Model):
-    """ Resume-Course association table"""   
-    __tablename__ = "resume_course"
-    course_pk = db.Column(db.Integer, db.ForeignKey('courses.course_pk'), primary_key = True, nullable=False)
-    course_res = db.Column(db.Integer, db.ForeignKey('resumes.course_res'), primary_key = True, nullable=False)
-    
-
-
-class ResumeProject(db.Model):
-    """ Resume-Project association table""" 
-    __tablename__ = "resume_project"
-    project_pk = db.Column(db.Integer, db.ForeignKey('projects.project_pk'), primary_key = True, nullable=False)
-    project_res = db.Column(db.Integer, db.ForeignKey('resumes.project_res'), primary_key = True, nullable=False)
-
-
-
-class ResumeTech(db.Model):
-    """ Resume-Technologies association table"""
-    __tablename__ = "resume_tech"
-    tech_pk = db.Column(db.Integer, db.ForeignKey('techs.tech_pk'), primary_key = True, nullable=False)
-    tech_res = db.Column(db.Integer, db.ForeignKey('resumes.tech_res'), primary_key = True, nullable=False)
-
-
-
-class ResumeWork(db.Model):
-    """ Resume-Work association table"""
-    __tablename__ = "resume_work"
-    work_pk = db.Column(db.Integer, db.ForeignKey('works.work_pk'), primary_key = True, nullable=False)
-    work_res = db.Column(db.Integer, db.ForeignKey('resumes.work_res'), primary_key = True, nullable=False)
-
+        return f"Added detail for {self.dytem.name}"
 
 ###########################################################
 
