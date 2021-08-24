08deb92d3083462798a67788b3e9bf37d94b3095
sharon zhen <sharonzhen@ucla.edu>
2021-08-24 00:14:46 +0000

    added htmls and updated server.py

--- server.py
+++ server.py
@@ -2,12 +2,67 @@
 
 from flask import (Flask, session, render_template, request,
                     flash, redirect)
-# from model import connect_to_db
-# import crud
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+from model import connect_to_db
+import crud
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.route('/')
+def show_homepage():
+    """ show application's homepage if user is in session
+        if user isn't in session, show login """
+    if session.get("user"):
+        return render_template('homepage.html')
+    else:
+        return render_template('login_create.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_session():
+    """ """
+    if session.get("user"):
+        return render_template('homepage.html')
+    
+
+@app.route('/create', methods=['POST'])
+def create_account():
+    """ """
+    if session.get("user"):
+        return render_template('homepage.html')
+    
+        
+
+
+@app.route('/profile')
+def view_contact():
+    if session.get("user"):
+        return render_template('profile.html')
+    else:
+        return render_template('login_create.html')
+
+
+@app.route('/resume')
+def view_resume():
+    if session.get("user"):
+        return render_template('resume.html')
+    else:
+        return render_template('login_create.html')
+
+
+@app.route('/generate')
+def pick_resume():
+    if session.get("user"):
+        return render_template('generate.html')
+    else:
+        return render_template('login_create.html')
+
+def generate_pdf():
+    pass
+
 
 
 if __name__ == "__main__":
