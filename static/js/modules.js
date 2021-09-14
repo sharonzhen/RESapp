016700b277ef976683e8962a6820dd5e39f178aa
sharon zhen <sharonzhen@ucla.edu>
2021-09-14 17:41:41 +0000

    modules.js and resume.jsx

---
+++ static/js/modules.js
@@ -0,0 +1,30 @@
+export async function safeGet(get_route) {
+    try {
+        let response = await fetch(get_route);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+        } catch(e) {
+        console.log(e);
+    }
+}
+
+
+export async function safePost(url, data) {
+    try {
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.text();
+    } catch(e) {
+        console.log(e);
+    }
+}
